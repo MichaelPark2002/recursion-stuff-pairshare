d5a6ba452db0b7b5670a2de078053bb9e16de5cb
adriantse136 <54153247+adriantse136@users.noreply.github.com>
2020-04-24 15:59:39 -0500

    wavelength

--- Recycle bin/WaveLength.java
+++ Recycle bin/WaveLength.java
@@ -1,13 +1,15 @@
 import java.util.Random;
+import java.text.DecimalFormat;
 public class WaveLength
 {
+    DecimalFormat df = new DecimalFormat("##.#####");
     /**
      * Smoothes out an array by averaging values with its neighbors.
      * @param values an array
      * @param size the number of elements in values
      * @return void
      */
-    
+
     /**
      * Prints an array
      * @param a an array
@@ -15,9 +17,9 @@ public class WaveLength
     public void print(double[] a)
     {
         for(double d:a)
-            System.out.println(d + " ");
+            System.out.print(df.format(d) + ", ");
     }
-    
+
     public void smooth(double[] values, int size)
     {
         for(int i = 0; i < size - 1; i++)
@@ -26,20 +28,20 @@ public class WaveLength
             values[i] = avg;
         }
     }
-    
+
     public static void main(String[] args)
     {
         final int size = 100;
         double[] a = new double[size];
         Random generator = new Random();
-        
+
         for(int i = 0; i < size; i++)
         {
             a[i] = 1 + i / 100.0;
             // Add some noise between -0.01 and .01
             a[i] = a[i] + (1 - 2 * (generator.nextDouble() * 1.0 / 10000)) / 100;
         }
-        
+
         WaveLength wave = new WaveLength();
         wave.print(a);
         wave.smooth(a, size);
